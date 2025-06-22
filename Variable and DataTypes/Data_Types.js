b86c140e1cffcd56d935dbe0ba1c99170f3ef881
guru <gurusaghi324@gmail.com>
2025-06-22 18:59:21 +0530

    commit

--- Variable and DataTypes/Data_Types.js
+++ Variable and DataTypes/Data_Types.js
@@ -57,5 +57,29 @@ let lastName = "Singh";
 
 var fullName = firstName + lastName;
 console.log(fullName);
+
 var fullName = firstName + " " + lastName;
 console.log(fullName);
+
+// JavaScript is Dynamically Typed Language
+
+a = 5
+console.log(typeof(a))
+a = "alpha"
+console.log(typeof(a))
+a = true
+console.log(typeof(a))
+
+// JavaScript is a forgiving language
+
+let num = 250
+num = 45;  // in  this case wihout semicolon js cant give error bcz js is forgiving lang
+
+let x = 4; x = 5; // in this case mendatory to use semicolon
+
+let a1 = "2" + 2
+let a2 = "5" - 2
+console.log(a1, typeof(a1)); // "22" automatically convert into string using common sence
+
+console.log(a2, typeof(a2)); // 3 bcz 5 turn into number
+
