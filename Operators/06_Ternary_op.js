793b2f5b6f3bc94a6cface0a4fbc8ff01f8c7df1
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-22 22:11:30 +0530

    Ternary Op

---
+++ Operators/06_Ternary_op.js
@@ -0,0 +1,8 @@
+// Ternary Operator : A shorthand for an if-else statement
+
+// Syntax: condition ? expressionIfTrue : expressionIfFalse
+
+let age = 18;
+let canVote = (age >= 18) ? "Eligible to vote" : "Not eligible to vote";
+
+console.log(canVote); // Output: Eligible to vote
