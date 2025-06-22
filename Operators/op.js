7b676af904d3efa7c28d81a8506504b1ffe6d7d7
guru <gurusaghi324@gmail.com>
2025-06-22 20:30:00 +0530

    Operator

---
+++ Operators/op.js
@@ -0,0 +1,18 @@
+// Operator : An operator is a symbol that tells the computer to do something with values.
+
+console.log(5 + 3);
+
+/* ‘+’ is the operator, it tells JavaScript to add 5 & 3.
+5 and 3 are the operands (value that an operator works on) */
+
+// Operands are the values.
+// Operators do something to those values
+
+// Types of Operators:
+// 1. Arithmetic Operators
+// 2. Assignment Operators
+// 3. Comparison Operators
+// 4. Logical Operators
+// 5. Unary Operators 
+// 6. Ternary Operator
+// 7. Bitwise Operators
