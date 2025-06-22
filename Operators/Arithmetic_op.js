337f223d50dd6a212e34258c3fe71a7df7cbe3f1
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-22 20:44:23 +0530

    Arithmetic Op

--- Operators/Arithmetic_op.js
+++ Operators/Arithmetic_op.js
@@ -1,1 +1,20 @@
 // Addition(+)
+
+let n1 = 5, n2 = 2;
+console.log(n1 + n2); 
+
+// Subtraction(-)
+console.log(n1 - n2); 
+
+// Multiplication(*)
+console.log(n1 * n2); 
+
+// Division(/)
+console.log(n1 / n2); 
+
+// Modulus(%)
+console.log(n1 % n2); // 1 (remainder of 10 divided by 3)
+
+// Exponentiation(**)
+console.log(n1 ** n2); // 8 (2 raised to the power of 3)
+
