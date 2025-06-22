bede12b2c51498ad09475633c84ce9e8f892e0ab
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-22 22:05:45 +0530

    Unary Op

---
+++ Operators/05_Unary_op.js
@@ -0,0 +1,13 @@
+// Unary Operator : Unary operators are operators that operate on a single operand. They can be used to perform operations such as negation, incrementing, or decrementing a value.
+
+// Increment Operator(++): The increment operator increases the value of a variable by one. It can be used in two forms: prefix (e.g., ++x) and postfix (e.g., x++).
+let x = 5;  
+console.log(++x); // Output: 6 (prefix increment)
+console.log(x++); // Output: 6 (postfix increment)
+console.log(x);   // Output: 7 (x is now 7 after postfix increment)
+
+// Decrement Operator(--): The decrement operator decreases the value of a variable by one. Similar to the increment operator, it can be used in prefix and postfix forms.
+let y = 5;
+console.log(--y); // Output: 4 (prefix decrement)
+console.log(y--); // Output: 4 (postfix decrement)
+console.log(y);   // Output: 3 (y is now 3 after postfix decrement
