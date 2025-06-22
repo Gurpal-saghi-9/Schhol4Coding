d79fa897bd1e85596d99b3662d1adeca6441d25f
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-22 20:58:23 +0530

    Arithmetic op

---
+++ Operators/02_Assignment_op.js
@@ -0,0 +1,28 @@
+// Assign (=) operator
+let a = 10; 
+console.log(a); // Outputs: 10
+
+// Add and Assign(+=) operator
+a += 5;
+console.log(a);
+
+// Subtract and Assign(-=) operator
+a -= 3;
+console.log(a); 
+
+// Multiply and Assign(*=) operator
+a *= 2;
+console.log(a);
+
+// Divide and Assign(/=) operator
+a /= 2;
+console.log(a);
+
+// Modulus and Assign(%=) operator
+a %= 3;
+console.log(a);
+
+// Exponentiation and Assign(**=) operator
+a = 5;
+a **= 2;
+console.log(a);
