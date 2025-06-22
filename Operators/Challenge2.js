9b8c105e9653a6b4ddc87ee0c0769dbe6f22d679
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-22 22:14:59 +0530

    Challange 2

---
+++ Operators/Challenge2.js
@@ -0,0 +1,10 @@
+/*
+On a booking website, check if the user's age is valid for booking:
+Age should be at least 18.
+Write a condition to check and display a message: "Eligible for booking" if the user is 18 or older. "Not eligible for booking" otherwise.
+*/
+let age = 18;
+
+let msg = age >= 18 ? "Eligible for booking" : "Not eligible for booking";
+
+console.log(msg); // Output: Eligible for booking
