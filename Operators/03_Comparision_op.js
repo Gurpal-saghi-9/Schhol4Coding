e51fc2ff0dac0a9e0a6ccf171145a67380eb21e7
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-22 21:13:52 +0530

    Comparision Op

---
+++ Operators/03_Comparision_op.js
@@ -0,0 +1,29 @@
+// Comparison Operators:- These help you compare values. They give true or false
+
+let x = 5;
+let y = '5';
+
+// Equal to (==) operator
+console.log(x == y); // Outputs: true (type coercion)
+
+// Strict Equal to (===) operator
+console.log(x === y); // Outputs: false (no type coercion)
+
+// Not Equal to (!=) operator
+console.log(x != y); // Outputs: false (type coercion)
+
+// Strict Not Equal to (!==) operator
+console.log(x !== y); // Outputs: true (no type coercion)
+
+// Greater than (>) operator
+console.log(x > 3); // Outputs: true
+
+// Less than (<) operator
+console.log(x < 10); // Outputs: true
+
+// Greater than or Equal to (>=) operator
+console.log(x >= 5); // Outputs: true
+
+// Less than or Equal to (<=) operator
+console.log(x <= 5); // Outputs: true
+
