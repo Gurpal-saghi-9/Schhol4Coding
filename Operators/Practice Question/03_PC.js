1200db831fac7cd041ee14e3f10151b3f0f910c4
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-23 11:22:22 +0530

    Practice Qs 3

---
+++ Operators/Practice Question/03_PC.js
@@ -0,0 +1,9 @@
+// 3. Get user to input a number using prompt and check whether even or odd using ternary operator.
+
+// let number = prompt("Please enter a number:");
+
+number = 6; // For testing purposes, you can replace this with a prompt in a real scenario
+
+let result = (number % 2 === 0) ? "Even" : "Odd";
+
+console.log(`The number ${number} is ${result}.`);
