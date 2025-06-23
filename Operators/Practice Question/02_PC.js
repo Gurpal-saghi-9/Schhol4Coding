a058be5dc055337eddb47a8011ea04e5bf111bc3
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-23 11:19:09 +0530

    Practice Qs 2

---
+++ Operators/Practice Question/02_PC.js
@@ -0,0 +1,9 @@
+// 2. Can you chain assignment operators?
+// // Yes, you can chain assignment operators in JavaScript. 
+
+let a = b = c = 50;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
