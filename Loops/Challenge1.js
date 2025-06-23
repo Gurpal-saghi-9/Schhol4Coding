0f0104a22c82063d219c5aac508aff3a4b653fbf
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-23 22:30:38 +0530

    Challenge 1

---
+++ Loops/Challenge1.js
@@ -0,0 +1,16 @@
+// Print all even numbers from 0 to 100
+
+console.log("Even number:");
+
+for(let i = 0; i <= 100; i++){
+   if(i % 2 == 0){
+      console.log(i);    
+   }
+}
+
+console.log("Odd number:");
+for(let i = 0; i <= 100; i++){
+   if(i % 2 != 0){
+    console.log(i);    
+   }
+}
