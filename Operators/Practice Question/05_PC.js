a220333dc237f96fb81c03e539281edca297fb35
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-23 11:29:22 +0530

    Practice Qs 5

---
+++ Operators/Practice Question/05_PC.js
@@ -0,0 +1,7 @@
+// 5. Check if a number is within a range between 10 and 20 (inclusive)
+
+let number = 15; // For testing purposes, you can replace this with a prompt in a real scenario
+
+let res = (number >= 10 && number <= 20) ? "The number is within the range." : "The number is outside the range.";
+
+console.log(res); // Output: The number is within the range.
