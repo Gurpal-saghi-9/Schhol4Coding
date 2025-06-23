065142365b673b19509f71b9148fda2d98019563
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-23 12:18:15 +0530

    Practic Qs 8

--- Operators/Practice Question/08_PC.js
+++ Operators/Practice Question/08_PC.js
@@ -1,8 +1,15 @@
 // 8. What will be the output of the following JavaScript code?
 
-let a = 5, b = 3, c = 2;
+let a = 5, b = 3, c = 3;
+
 
 let result = a++  + --b * c-- - ++a + b-- / --c;
+            //   5   +   2 * 3   -  7  + 2   /   1
+            //   5   +     6     -  7  +    2 
+            //       11          -  7  +    2
+            //                   4     +    2
+            //                         6 
+
 
 console.log(`a : ${a}`);
 console.log(`b : ${b}`);
