ba6605ff0622807352c6d12b609be467111ea553
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-23 21:21:23 +0530

    Basics

---
+++ Loops/basics.js
@@ -0,0 +1,22 @@
+// Loops in JavaScript are a way to repeat a block of code multiple times.
+
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+
+// If we wan't to do one procedure many time than we can use and it runs the time when the condition is true
+
+for (let i = 0; i < 5; i++) {
+    console.log("Hello")
+}
+
+/*
+Types of Loops in JavaScript:-
+1. for
+2. while
+3. do-while
+4. for-of (arrays, strings, maps or sets [don’t care about index])
+5. for-in (iterating over the properties (keys) of an object or the indexes of an array)
+*/
