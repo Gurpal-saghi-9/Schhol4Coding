61bdbf2b67f8b85e88eabfe1ad4c6f568a1fc292
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-23 13:10:46 +0530

    Basics

---
+++ Conditionals/basic.js
@@ -0,0 +1,31 @@
+/*
+A Conditional statement lets your code make decisions. It checks if something is true orfalse, and then runs certain code based on that.
+
+Syntax:
+if (condition) {
+    // code to run if the condition is true
+} else {
+    // code to run if the condition is false
+}
+
+Example:
+"If it's raining, take an umbrella. Otherwise, enjoy the sunshine!"
+*/
+
+let wheather = "rainy";
+
+if (wheather === "rainy") {
+    console.log("Take an umbrella");
+} else {
+    console.log("Enjoy the sunshine!");
+}
+
+/* 
+Different typpes of Conditional Statements:
+1. if statement
+2. if...else statement
+3. if...else if...else statement
+4. Nested if statement
+5. switch statement
+6. Ternary Operator [?:] (short form)
+*/
---
+++ Conditionals/new.js
@@ -0,0 +1,11 @@
+// Scenario: On an e-commerce website, show a message when product is out of stock
+
+// Question: Write a program to check if the stock of a product is 0. If it is, display the message "Product is out of stock.".
+
+let productStock = 0;
+
+if (productStock === 0) {
+    console.log("Product is out of stock.");    
+}else{
+    console.log("Product is in stock");
+}
