f9e41db189c6628c7bfb5b98003f2e2b05b79f83
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-24 11:12:31 +0530

    Without Parameter & with Return Value

--- Functions/function_return_only.js
+++ Functions/function_return_only.js
@@ -6,3 +6,33 @@ function getAppName() {
 
 let name = getAppName();
 console.log(name);  // Output: Desi Devlogs
+
+// Return Website Name
+function getSiteName(){
+    return "Desi Devlogs"
+}
+console.log(getSiteName());
+
+// Return Current Year
+function getYear(){
+    return new Date().getFullYear()
+}
+console.log(getYear());
+
+// Return a Random Number (1–10)
+function getRandomNumber(){
+    return Math.floor(Math.random()*10)
+}
+console.log(getRandomNumber());
+
+// Return App Version
+function getVersion(){
+    return "v1.0.0"
+}
+console.log(getVersion());
+
+// Return a Motivational Quote
+function getQuote(){
+    return "Keep pushing forward, Guru!"
+}
+console.log(getQuote());
