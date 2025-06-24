2028df12564fb4421ab6a5eb9be1f16271df2f02
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-06-24 13:14:16 +0530

    Practice Qs 1

---
+++ Functions/Practice Question/01_PC.js
@@ -0,0 +1,7 @@
+// Write a regular function that takes a string and returns it with the first letter capitalized.
+
+function capitalizedFirstLetter(str){
+    console.log(str.charAt(0).toUpperCase() + str.slice(1));
+    
+}
+capitalizedFirstLetter("gurupal singh");
