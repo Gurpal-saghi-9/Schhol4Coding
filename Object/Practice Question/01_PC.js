c983d2570f410f2706759a7660eba2f691bef745
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-07-15 08:26:59 +0530

    Practice Qs.

--- Object/Practice Question/01_PC.js
+++ Object/Practice Question/01_PC.js
@@ -1,6 +1,25 @@
 /*
 Create a person object with properties: name, age, and city. Then
-a) Log each property as: value of “name” property is “manas” using loop.
+a) Log each property as: value of “name” property is “Gurpal Singh” using loop.
 b) Add a new property called email to the person object.
 c) Delete “city” property from person object.
 */
+
+const person = {
+  name: "Gurpal Singh",
+  age: 21,
+  city: "New York",
+};
+
+// a) Log each property
+for (const key in person) {
+  console.log(`value of "${key}" property is "${person[key]}"`);
+}
+
+// b) Add a new property called email
+person.email = "Gurusaghi324@gmail.com";
+
+// c) Delete "city" property
+delete person.city;
+
+console.log("Updated person object:", person);
