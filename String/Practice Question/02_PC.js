f2f36bae61b45ce08b6e3504dd35d5ba104ea9c3
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-08-17 19:02:00 +0530

    PQ 2

--- String/Practice Question/02_PC.js
+++ String/Practice Question/02_PC.js
@@ -1,3 +1,10 @@
+/*
+2. Take a string and a character from the user and:
+    a) count how many times that character appears in the string.
+    b) Case-Insensitive Version
+    c) Find All Occurrence Positions
+*/
+
 let str = "I Am Gurpal Singh Saghi";
 let char = "a";
 
