8d99237a20d5636022391425215c06f24cf61946
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-08-18 11:56:56 +0530

    PQ 6

--- Array/Practice Question/06_PC.js
+++ Array/Practice Question/06_PC.js
@@ -1,2 +1,11 @@
 // 6. We have three variables a, b, c, a contains any number, b contains any string, c contains any object, and d contains any array. Can we create an array from all these four variables? If yes, How?
 
+let a = 1
+let b = "guru"
+let c = {
+    name : "Gurpal",
+    age : 21
+}
+let d = [1, 2, 3, 4, 5]
+
+console.log(Array.of(a, b, c, d));
