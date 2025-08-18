7414a85e4874287c1f57af9e6b78fb837839f35c
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-08-18 11:54:59 +0530

    PQ 5

---
+++ Array/Practice Question/06_PC.js
@@ -0,0 +1,2 @@
+// 6. We have three variables a, b, c, a contains any number, b contains any string, c contains any object, and d contains any array. Can we create an array from all these four variables? If yes, How?
+
