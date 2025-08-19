c901847d73ea8833588f4ee0239fe1ab8ad1b788
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-08-19 18:52:58 +0530

    PQ 4

--- Object/Practice Question/04_PC.js
+++ Object/Practice Question/04_PC.js
@@ -10,18 +10,33 @@ const users = [
 const users = [
   { name: "Rahul", role: "admin" },
   { name: "Abhishek", role: "user" },
+  { name: "Guru", role: "SuperAdmin" },
   { name: "Simran", role: "admin" },
   { name: "Ahil", role: "user" },
   { name: "Minakshi", role: "admin" },
+  { name: "Raj", role: "SuperAdmin" },
+  { name: "Deep", role: "user" },
 ];
 
-function getAdminNames(userArray) {
-  return userArray
-    .filter(user => user.role === "admin")
-    .map(admin => admin.name);
+// let filtered_item = users.filter((item) => {
+//   return item.role === "admin";
+// });
+
+// let result = filtered_item.map((item) => {
+//   return item.name;
+// });
+// console.log(result);
+
+function filteredArray(item, role) {
+  let filtered_item = users.filter((item) => {
+    return item.role === role;
+  });
+
+  let result = filtered_item.map((item) => {
+    return item.name;
+  });
+  return result;
 }
 
-// Example usage:
-const adminNames = getAdminNames(users);
-console.log(adminNames); // Output: ["Alice", "Charlie"]
-
+let result_Array = filteredArray(users, "admin");
+console.log(result_Array);
