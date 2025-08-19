12ad27270826a2f4e597e2b2fd0c1f3ffe4b1463
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-08-19 19:17:09 +0530

    PQ 6

--- Object/Practice Question/06_PC.js
+++ Object/Practice Question/06_PC.js
@@ -11,3 +11,26 @@ const comments = [
 2 :["Thanks for sharing!"]
 }
 */
+
+const comments = [
+    { postId: 1, text: 'Greet Post!' },
+    { postId: 2, text: 'Thanks for sharing!' },
+    { postId: 1, text: 'Very helpfull' },
+    { postId: 3, text: 'Awesome bruh...!' },
+];
+
+function groupByPost(comments){
+    let groupedComments = {}
+
+    comments.forEach(element => {
+        if (groupedComments.hasOwnProperty((element.postId))){
+            groupedComments[element.postId].push(element.text)
+        }else{
+            groupedComments[element.postId] = [element.text]
+        }
+    });
+    console.log(groupedComments)
+
+}
+
+groupByPost(comments)
