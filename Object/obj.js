4a8a152731a130e6a2a0c095f118abf86237c5fe
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-08-19 17:50:30 +0530

    object

---
+++ Object/obj.js
@@ -0,0 +1,49 @@
+let car = {
+    brand: "Lamborghini",
+    color: "red",
+    year: 2020,
+    speed: 120,
+    'full name': "Lamborghini Aventador",
+    start: function() {
+        console.log("The car has started.");    
+        return 'started';
+    },
+    stop: () => {
+        console.log("The car has stopped.");
+        return 'stopped';
+    }
+}
+
+// console.log(car);
+// console.log(car.color);
+// car.stop();
+// console.log(car.stop());
+
+// console.log(car['brand']);
+// car['start']();
+
+// console.log(car['full name']);
+
+// object using object constructor
+// let data = new Object();
+// data.name = 'Guru';
+// data.age = 2023;
+// data.city = 'Bangalore';
+// data.state = 'Karnataka';
+
+// console.log(data);
+
+// delete data.age;
+// console.log(data);
+
+// let obj = {
+//     name : "alpha",
+//     greet: function (){
+//         console.log(`Hello, my name is ${this.name}`);  
+//     }
+// }
+
+// obj.greet();
+
+
+// ----------------------- ------------------------
