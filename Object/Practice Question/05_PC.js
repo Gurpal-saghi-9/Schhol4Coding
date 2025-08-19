19bc6619015e336ec10f771b31c61a03766de76f
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-08-19 19:03:12 +0530

    PQ 5

--- Object/Practice Question/05_PC.js
+++ Object/Practice Question/05_PC.js
@@ -2,3 +2,24 @@
 Write a function searchProducts(products, keyword) that returns an array of products whose name
 includes the given keyword (case-insensitive)
 */
+
+function searchProducts(products, keyword) {
+    let result = products.filter(function(product) {
+        return product.name.toLowerCase().includes(keyword.toLowerCase());
+    });
+    
+    return result; 
+}
+
+let products = [
+    {id: 1, name: "Samsung"},
+    {id: 2, name: "Realme"},
+    {id: 3, name: "Redmi"},
+    {id: 4, name: "Nothing"},
+    {id: 5, name: "Oppo"},
+    {id: 6, name: "Vivo"}
+];
+
+let matchedProducts = searchProducts(products, "m");
+
+console.log(matchedProducts);
