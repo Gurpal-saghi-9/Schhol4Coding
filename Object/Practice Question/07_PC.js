70a65001d19f5eb12f2d34bb4097d20552159e91
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-08-19 19:29:13 +0530

    PQ 7

--- Object/Practice Question/07_PC.js
+++ Object/Practice Question/07_PC.js
@@ -1,5 +1,28 @@
 /*
 7. Write a function buildQuery(params) that returns
 a query string from an object of parameters.
-const params = { name: 'John', age: 30, city: 'New York' };
 */
+
+let params = {
+    search : "laptop",
+    page : 2,
+    sort : "price"
+}
+
+function createURL(params){
+    // return `search=${params.search}&page=${params.page}&sort=${params.sort}`
+
+    let url = "";
+    for (let key in params){
+        url = url + `${key}=${params[key]}&`
+    }
+    let finalURL = url.slice(0,url.length-1)
+    return finalURL 
+    
+    
+}
+
+let fullURL = "https://example.com?" + createURL(params);
+console.log(fullURL);
+
+
