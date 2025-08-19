ac44f44b56d8611567b9a48fa5dd405eca0526ca
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-08-19 18:31:28 +0530

    Destructing

---
+++ Object/destructuring.js
@@ -0,0 +1,22 @@
+let person = {
+    name : "Guru",
+    age : 21,
+    city : "Sirhind"
+}
+
+const {name, age, city} = person
+
+// console.log(name)
+// console.log(age)
+// console.log(city)
+
+
+let arr = [1, "Str", {name : "ram"}]
+
+let [num, string, object] = arr
+
+// console.log(num)
+// console.log(string)
+// console.log(object)
+// console.log(typeof object)
+
